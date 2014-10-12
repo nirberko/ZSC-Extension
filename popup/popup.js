87cda6e90ad133dda587771d21e9a7dc66259133
leibale <l.eidelman@self-point.com>
2014-10-12 15:48:10 +0300

    fix merge

--- popup/popup.js
+++ popup/popup.js
@@ -6,11 +6,7 @@ $(function () {
 
     chrome.storage.local.get(function (storage) {
         if (!storage.numberTopics) {
-<<<<<<< HEAD
             chrome.storage.local.set({numberTopics: 5}, function() {
-=======
-            chrome.storage.local.set({numberTopics: 3}, function() {
->>>>>>> origin/master
                 refresh();
             });
         } else {
@@ -22,24 +18,18 @@ $(function () {
 });
 
 function refresh() {
-<<<<<<< HEAD
-=======
-    $("#lastTopics ul").html('');
->>>>>>> origin/master
+    var $ul = $("#lastTopics ul");
+
+    $ul.html('');
     $.get('http://www.zsc.co.il/index.php/rss/forums/1-zsc-rss/', function (data) {
         var items = $(data).find('item');
 
         chrome.storage.local.get(function (storage) {
-<<<<<<< HEAD
-            $("#lastTopics ul").html('');
-
-=======
->>>>>>> origin/master
             for (var i = 0; i < storage.numberTopics; i++) {
                 var title = $(items[i]).find('title').text();
                 var link = $(items[i]).find('link').text();
 
-                $("#lastTopics ul").append('<li><a target="_blank" href="' + link + '">' + title + '</a></li>');
+                $ul.append('<li><a target="_blank" href="' + link + '">' + title + '</a></li>');
             }
         });
     });
