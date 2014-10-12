9b72daa0ae4876ebf060f3711371c17467839799
leibale <l.eidelman@self-point.com>
2014-10-12 07:38:20 +0300

    add auto fill options

--- options/options.js
+++ options/options.js
@@ -1,8 +1,14 @@
 $(function() {
+    chrome.storage.local.get(function(storage) {
+        var $form = $('form');
+        $form.find('input[name=showNotifications]').prop('checked', storage.showNotifications)
+        $form.find('input[name=updateTime]').val(storage.updateTime);
+    });
+
     $('form').submit(function(event) {
         var $this = $(this);
         chrome.storage.local.set({
-            showNotifications: $this.find('input[name=updateTime]').prop('checked'),
+            showNotifications: $this.find('input[name=showNotifications]').prop('checked'),
             updateTime: Number($this.find('input[name=updateTime]').val()) * 1000
         });
 
