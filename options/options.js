2f446f16737bd3c1f441f8f03764a8b4d1e813b5
leibale <l.eidelman@self-point.com>
2014-10-12 07:52:15 +0300

    add refresh button and add options set number topics

--- options/options.js
+++ options/options.js
@@ -3,13 +3,15 @@ $(function() {
         var $form = $('form');
         $form.find('input[name=showNotifications]').prop('checked', storage.showNotifications)
         $form.find('input[name=updateTime]').val(storage.updateTime);
+        $form.find('input[name=numberTopics]').val(storage.numberTopics);
     });
 
     $('form').submit(function(event) {
         var $this = $(this);
         chrome.storage.local.set({
             showNotifications: $this.find('input[name=showNotifications]').prop('checked'),
-            updateTime: Number($this.find('input[name=updateTime]').val())
+            updateTime: Number($this.find('input[name=updateTime]').val()),
+            numberTopics: Number($this.find('input[name=numberTopics]').val())
         });
 
         event.preventDefault();
--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,34 @@
 $(function () {
-    $.get('http://www.zsc.co.il/index.php/rss/forums/1-zsc-rss/', function (data) {
-        var items = $(data).find('item');
-
-        for (var i = 0; i < 5; i++) {
-            var title = $(items[i]).find('title').text();
-            var link = $(items[i]).find('link').text();
-
-            $("#lastTopics ul").append('<li><a target="_blank" href="' + link + '">' + title + '</a></li>');
-        }
-    });
-
     $.get('/manifest.json', function (data) {
         window.manifest = JSON.parse(data);
         $('#version').text('גרסא ' + window.manifest.version);
     });
+
+    chrome.storage.local.get(function (storage) {
+        if (!storage.numberTopics) {
+            chrome.storage.local.set({numberTopics: 3}, function() {
+                refresh();
+            });
+        } else {
+            refresh();
+        }
+    });
+
+    $('#refresh').click(refresh);
 });
+
+function refresh() {
+    $("#lastTopics ul").html('');
+    $.get('http://www.zsc.co.il/index.php/rss/forums/1-zsc-rss/', function (data) {
+        var items = $(data).find('item');
+
+        chrome.storage.local.get(function (storage) {
+            for (var i = 0; i < storage.numberTopics; i++) {
+                var title = $(items[i]).find('title').text();
+                var link = $(items[i]).find('link').text();
+
+                $("#lastTopics ul").append('<li><a target="_blank" href="' + link + '">' + title + '</a></li>');
+            }
+        });
+    });
+}
